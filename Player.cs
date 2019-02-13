f7c067b80d54c1013cd02f82a7b81cbd35d70dfd
Mohammed Alzubaidi <s201708758@aou.edu.sa>
2019-02-13 22:47:05 +0300

    Thoes are two scripts for (Moving the player) & another script for (Moving the Camera)

---
+++ Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float movementSpeed = 10;
+    public float turningSpeed = 60;
+
+    void Update()
+    {
+
+
+        //The orginal script should look like this: 
+        ////float horizontal = Input.GetAxis("Horizontal") * turningSpeed * Time.deltaTime;
+        ////transform.Rotate(0, horizontal, 0);
+        //but I edit so it can be working with the MouseCamera script
+
+        float horizontal = Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime;
+        transform.Translate(horizontal, 0,0 );
+
+        float vertical = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
+        transform.Translate(0, 0, vertical);
+    }
+}
